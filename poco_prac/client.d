52ea24b2d5adc15c615361ce4c92dc7ac2aaa940
Chris Walsh <chris.is.rad@pm.me>
2025-02-24 06:27:15 -0500

    updates and such

--- poco_prac/client.d
+++
@@ -1,2 +0,0 @@
-client.o: client.cpp inc/outils.hpp
-inc/outils.hpp:
--- poco_prac/outils.cpp
+++
@@ -1,7 +0,0 @@
-#include "inc/outils.hpp"
-
-namespace odds {
-    bool startswith(const std::string &s, const std::string &what) {
-        return s.find(what) == 0;
-    }
-}
